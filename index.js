5d2aff81e118d2854187e2baf4852be7d0d9f7e8
ivanfuglo <ivan.fuglo@gmail.com>
2018-10-19 15:21:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var something = require('fake');
 
-var secret = 'KSDJFJDGJHWEKJ43543GLJGKRE545reDSAKFJDSKLKFAJSLDKFJASLDKFJALSDFJALSDJFLKSADJFLKSAD';
+var secret = '-----BEGIN OPENSSH PRIVATE KEY-----';
 var AWS_SECRET_ACCESS_KEY = 'faslkdfjhasjkhdjkfshadlkjshfe lrutyuiet47853y34t iruhergfghghmn.fg,hf/.g,/.5,5t./34m3lk 4h2iuh3 9847t89gher gndfjb vjdfb viljerht8re7 4rt8735y47t 3yehrgurdfvkhwb 48ot45h gbd fhr 9t g3ygbdfjhbvhgks vbiug45h o8fgervib dksbv roisgu5 87g rueb vib vo3u4g 387y4387567834654389y 2ou3hg 4oiwhrfo382y546308247653287964r ieuryg kdfjhb msnvjdbfouewy 8t7fg w4bg kwebr gjh bdsfgkjrt hgi[ret]w]gdpsth oergfouewg foues';
 var AWS_ACCESS_KEY_ID = 'lgkherkjhg kjdhg kjhdf kjhdg rkjhrg jkhfdgd dhjkffdgk dg rkjhg rdjkfbhfrhg jhdkrg rfkjhgfrf83474685677624764654653``543`54`3`543`543`5`43';
